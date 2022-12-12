c014f72b614edfa10dde41a4166ed1a058bdf055
Binod Rawat <binodrawat@cloud.upwork.com>
2022-12-12 16:43:36 +0530

    changes for error

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,29 +3,31 @@ import './index.css';
 
 export const Modal = ({ keyValue, data, setData, setIsOpen }) => {
     const [formData, setFormData] = useState({});
+    const [error, setError] = useState();
     const formModel = [{
         key: 'name',
+        type: 'text',
         validation: [{
             type: 'required',
             message: 'Name field is required',
         }],
     },{
         key: 'email',
+        type: 'email',
         validation:[{
-            type: 'required',
-            message: 'Email field is required',
-        }, {
             type: 'email',
             message: 'Invalid email'
         }]
     },{
         key: 'phone',
+        type: 'text',
         validation: [{
             type: 'required',
             message: 'Phone field is required',
         }],
     },{
         key: 'website',
+        type: 'text',
         validation: [{
             type: 'required',
             message: 'Website field is required',
@@ -37,7 +39,7 @@ export const Modal = ({ keyValue, data, setData, setIsOpen }) => {
         setFormData(clickData[0])
     }, [keyValue])
     
-    const onSave = () => {
+    const onSave = (e) => {
         let newArray = data.map((entry) => {
                 if (entry.id === formData.id) return { ...formData };
                 return entry;
@@ -51,9 +53,10 @@ export const Modal = ({ keyValue, data, setData, setIsOpen }) => {
         ...formData,
         [objKey]: e.target.value
      };
+     setError(Object.values(obj).includes(''));
      setFormData(obj);
     };
-    
+
     return (
         <div className='popup'>
             <div className="modal-content">
@@ -61,24 +64,29 @@ export const Modal = ({ keyValue, data, setData, setIsOpen }) => {
                     <h4>Edit Modal</h4>
                     <button className="btn-close" onClick={(e) => setIsOpen(false)}></button>      
                 </div>
-                <div className="modal-body">
-                    {
-                        formModel.map(form => {
-                            return (
-                            <div class="row mb-3" key={form.key}>
-                                <div className='col-md-3 txt-align'><span class="text-danger">*</span><span class="text-capitalize">{form.key}</span>:</div>
-                                <div className='col-md-9'><input type="text" class="form-control" value={formData[form.key]} onChange={(e) => handleChange(e, form.key)} />
+                <form class="was-validated">
+                    <div className="modal-body">
+                        {
+                            formModel.map(form => {
+                                return (
+                                <div class="row mb-3" key={form.key}>
+                                    <div className='col-md-3 txt-align'><span class="text-danger">*</span><span class="text-capitalize">{form.key}</span>:</div>
+                                    <div className='col-md-9'><input type={form.type} class="form-control" value={formData[form.key]} onChange={(e) => handleChange(e, form.key)} name={form.key} required/>
+                                    {form.validation.map((row) => {
+                                        return <div class="invalid-feedback">{row.message}</div>}
+                                    )}
+                                    </div>
                                 </div>
-                            </div>
-                        )})
-                    }
-                </div>
+                            )})
+                        }
+                    </div>
                 <div className="modal-footer">
                     <div className="modal-button">
                         <button type="button" className="btn btn-outline-secondary" onClick={(e) => setIsOpen(false)} >Cancel</button>
-                        <button type="button" className="btn btn-primary" onClick={(e) => onSave()}>Ok</button>   
+                        <button type="submit" className="btn btn-primary" disabled={error} onClick={(e) => onSave(e)}>Ok</button>   
                     </div>
                 </div>
+                </form>
             </div>
         </div>
     )
